b8e7ec81a8a707c710b74e8847902ede5f5c3075
Rostislav <rkuzmenko2000@gmail.com>
2024-09-15 23:28:34 +0300

    Added exercise to JavaAdvanced/SetsAndMapsAdvanced

---
+++ JavaAdvanced/SetsAndMapsAdvanced/src/Largest3Numbers_09.java
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Largest3Numbers_09 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        Arrays.stream(scanner.nextLine().split("\\s+"))
+                .map(Integer::parseInt)
+                .sorted((left, right) -> right.compareTo(left))
+                .limit(3)
+                .forEach(number -> System.out.printf("%d ", number));
+    }
+}
