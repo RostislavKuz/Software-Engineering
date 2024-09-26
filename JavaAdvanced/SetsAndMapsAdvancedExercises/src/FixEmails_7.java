69d8076651aec77bc5ec7168c3de1ce91ee9f1ae
Rostislav <rkuzmenko2000@gmail.com>
2024-09-26 22:30:23 +0300

    Added exercise

---
+++ JavaAdvanced/SetsAndMapsAdvancedExercises/src/FixEmails_7.java
@@ -0,0 +1,24 @@
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class FixEmails_7 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        Map<String, String> emailsData = new LinkedHashMap<>();
+        String name = scanner.nextLine();
+
+        while (!name.equals("stop")) {
+            String email = scanner.nextLine();
+
+            if (!email.endsWith("uk") && !email.endsWith("us") && !email.endsWith("com")) {
+                emailsData.put(name, email);
+            }
+            name = scanner.nextLine();
+        }
+
+        emailsData.entrySet().forEach(e -> System.out.printf("%s -> %s%n", e.getKey(), e.getValue()));
+    }
+}
