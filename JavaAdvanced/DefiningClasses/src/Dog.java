1af084df82b4f385a6ac064a72d2a19abfb59060
Rostislav <rkuzmenko2000@gmail.com>
2024-11-10 23:56:27 +0200

    Added exercise

---
+++ JavaAdvanced/DefiningClasses/src/Dog.java
@@ -0,0 +1,24 @@
+public class Dog {
+    private String name;
+    private int age;
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void bark(){
+        System.out.println("Bark bark");
+    }
+}
---
+++ JavaAdvanced/DefiningClasses/src/Main.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        Dog dog = new Dog();
+        dog.setName("Robert");
+        int dogAge = dog.getAge();
+
+        System.out.println(dog.getAge()f);
+
+    }
+}
