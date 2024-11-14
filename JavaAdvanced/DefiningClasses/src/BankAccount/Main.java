8aaeb471fd6f67e7cf6acc759bd63e7facdf524f
Rostislav <rkuzmenko2000@gmail.com>
2024-11-14 22:13:45 +0200

    added exercise

---
+++ JavaAdvanced/DefiningClasses/src/BankAccount/Main.java
@@ -0,0 +1,10 @@
+package BankAccount;
+
+public class Main {
+    public static void main(String[] args) {
+
+        BankAccount bankAccount = new BankAccount();
+        System.out.println(BankAccount.getInterestRate());
+
+    }
+}
