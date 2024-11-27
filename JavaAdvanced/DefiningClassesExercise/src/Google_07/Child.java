5adfc1322a70100560f91024087ccc9a22e1f5e7
Rostislav <rkuzmenko2000@gmail.com>
2024-11-27 22:54:57 +0200

    Added classes

---
+++ JavaAdvanced/DefiningClassesExercise/src/Google_07/Child.java
@@ -0,0 +1,11 @@
+package Google_07;
+
+public class Child {
+    private String childName;
+    private String childBirthday;
+
+    public Child(String childName, String childBirthday) {
+        this.childName = childName;
+        this.childBirthday = childBirthday;
+    }
+}
---
+++ JavaAdvanced/DefiningClassesExercise/src/Google_07/Main.java
@@ -0,0 +1,23 @@
+package Google_07;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String information = scanner.nextLine();
+        while (!information.equals("End")){
+
+
+
+
+
+            information = scanner.nextLine();
+        }
+
+
+
+
+    }
+}
