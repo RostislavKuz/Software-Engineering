effbbc80125e46c8b023839e175ac5172010c745
Rostislav <rkuzmenko2000@gmail.com>
2024-12-12 21:29:10 +0200

    Added some methods

--- JavaAdvanced/WorkshopExercise/src/customLinkedList/DoublyLinkedList.java
+++ JavaAdvanced/WorkshopExercise/src/customLinkedList/DoublyLinkedList.java
@@ -18,6 +18,61 @@ public class DoublyLinkedList {
         this.size++;
     }
 
+    public void addLast(int value) {
+        Node newNode = new Node(value);
+        if (isEmpty()) {
+            this.head = newNode;
+            this.tail = newNode;
+        } else {
+            this.tail.next = newNode;
+            newNode.prev = this.tail;
+            this.tail = newNode;
+        }
+        this.size++;
+    }
+
+    public int get(int index) {
+        int result = 0;
+        checkValidIndex(index);
+        if (index <= this.size / 2) {
+            Node currentNode = this.head;
+            for (int i = 0; i < index; i++) {
+                currentNode = currentNode.next;
+            }
+            result = currentNode.currentValue;
+        } else {
+            Node currentNode = this.tail;
+            for (int i = this.size - 1; i > index; i--) {
+                currentNode = currentNode.prev;
+            }
+            result = currentNode.currentValue;
+        }
+        return result;
+    }
+
+    public int removeFirst() {
+        int removedElement;
+        if (isEmpty()) {
+            throw new IllegalStateException("Error while remove element from empty list!");
+        } else if (this.size == 1) {
+            removedElement = this.head.currentValue;
+            this.head = this.tail = null;
+            size--;
+        } else {
+            removedElement = this.head.currentValue;
+            this.head = this.head.next;
+            this.head.prev = null;
+            this.size--;
+        }
+        return removedElement;
+    }
+
+    private void checkValidIndex(int index) {
+        if (index < 0 || index >= this.size) {
+            throw new IndexOutOfBoundsException("The index " + index + " is out of bounds!");
+        }
+    }
+
     public boolean isEmpty() {
         // true -> empty
         // false -> not empty
