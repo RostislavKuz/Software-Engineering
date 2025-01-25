33244bad07c35b14a8d6e87149fa862fb70bb0a3
Rostislav <rkuzmenko2000@gmail.com>
2025-01-25 22:42:12 +0200

    Added exercise

---
+++ JavaOOP/InheritanceExercise/src/hero_03/DarkWizard.java
@@ -0,0 +1,8 @@
+package hero_03;
+
+public class DarkWizard extends Wizard{
+
+    public DarkWizard(String username, int level) {
+        super(username, level);
+    }
+}
---
+++ JavaOOP/InheritanceExercise/src/hero_03/Main.java
@@ -0,0 +1,13 @@
+package hero_03;
+
+public class Main {
+    public static void main(String[] args) {
+
+        BladeKnight bladeKnight = new BladeKnight("Ash", 12);
+        DarkKnight darkKnight = new DarkKnight("Dark", 111);
+
+        System.out.println(bladeKnight);
+
+        System.out.println(darkKnight);
+    }
+}
