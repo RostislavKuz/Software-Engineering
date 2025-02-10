7fa372d28c6460c0466fd97dd9c20aa9b3ea5248
Rostislav <rkuzmenko2000@gmail.com>
2025-02-10 19:03:25 +0200

    Added exercise

---
+++ JavaOOP/Polymorphism/src/mathOperation_01/Main.java
@@ -0,0 +1,10 @@
+package mathOperation_01;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println(MathOperation.add(1, 2));
+        System.out.println(MathOperation.add(1, 2, 3));
+        System.out.println(MathOperation.add(1, 2, 3, 4));
+    }
+}
